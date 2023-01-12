4229014b52c1e25eed2123cb601868bc9e5e338b
Kyle Robers <kyl.rober@gmail.com>
2023-01-12 01:40:07 -0700

    implemented model for save board method

--- server/models/boards.js
+++ server/models/boards.js
@@ -1,11 +1,22 @@
-// const db = require('./db/index.js');
+const db = require('../db/index.js');
 
 function loadBoard(callback) {
 
 }
 
-function postBoard(data, callback) {
+function saveBoard(data, callback) {
   console.log('data in models post is ', data);
+  const numString = data.numbers.toString();
+  const resString = data.resources.toString();
+  const colString = data.colors.toString();
+  console.log('string is ', numString, resString, colString);
+  db.query(`INSERT INTO boards (numbers, resources, colors) VALUES ('${numString}', '${resString}', '${colString}')`)
+    .then((res) => {
+      callback(null, res);
+    })
+    .catch((err) => {
+      callback(err, null);
+    })
 }
 
-module.exports = { loadBoard, postBoard };
+module.exports = { loadBoard, saveBoard };
