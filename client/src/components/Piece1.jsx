8bdcf199e7083d06dadc5c96c2d63b4115ae7c63
Kyle Robers <kyl.rober@gmail.com>
2023-01-12 01:42:48 -0700

    implemented background images for each tile relating to its resource

--- client/src/components/Piece1.jsx
+++ client/src/components/Piece1.jsx
@@ -10,12 +10,36 @@ function Piece1({number, resource, color}) {
   }
   adjustXPos();
 
+  let image = "";
+  const setImage = () => {
+    if (resource === 'sheep') {
+      image = '../assets/Type01.png'
+    }
+    if (resource === 'stone') {
+      image = '../assets/Type02.png'
+    }
+    if (resource === 'wood') {
+      image = '../assets/Type03.png'
+    }
+    if (resource === 'wheat') {
+      image = '../assets/Type04.png'
+    }
+    if (resource === 'brick') {
+      image = '../assets/Type05.png'
+    }
+    if (resource === 'desert') {
+      image = '../assets/Type06.png'
+    }
+  }
+  setImage();
+
   return (
     <span className="piece">
       <Hexagon
-        style={{stroke: 'wheat', fill: `${color}`}}
+        style={{stroke: 'wheat', strokeOpacity: 0}}
+        backgroundImage={image}
         className="hex">
-        <text x={xPos} y="57.5%" fill="black">{number}</text>
+        <text x={xPos} y="57.5%" fill="white">{number}</text>
       </Hexagon>
     </span>
   );
