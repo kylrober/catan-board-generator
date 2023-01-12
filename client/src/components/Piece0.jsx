35d8635bfb4cfe82bcef9c0f512881d3dc308b48
Kyle Robers <kyl.rober@gmail.com>
2023-01-11 19:08:34 -0700

    implemented hex tiles instead of squares. fixed positioning of text inside

--- client/src/components/Piece0.jsx
+++ client/src/components/Piece0.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Hexagon from 'react-hexagon'
 
 function Piece0({number, resource, color}) {
+  let xPos = '42.5%';
+  const adjustXPos = () => {
+    if (number > 9) {
+      xPos = '32.5%';
+    }
+  }
+  adjustXPos();
+
   return (
-    <span className="p0" style={{background: `${color}`}}>
-      {number} {resource}
+    <span className="piece">
+      <Hexagon
+        style={{stroke: 'wheat', fill: `${color}`}}
+        className="hex">
+        <text x={xPos} y="57.5%" fill="black">{number}</text>
+      </Hexagon>
     </span>
   );
 }
