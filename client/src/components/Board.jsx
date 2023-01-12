3f9545ae62d88ab1508fd2ac4133b4959314413c
Kyle Robers <kyl.rober@gmail.com>
2023-01-11 19:08:01 -0700

    removed breaks from rows to format for hexs

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -26,37 +26,33 @@ function Board( {numbers, resources, colors} ) {
   return (
     <div className="board">
       <div className="row">
-      < Piece0 className="piece" number={numbers[0]} resource={resources[0]} color={colors[0]}/>
-      < Piece1 number={numbers[1]} resource={resources[1]} color={colors[1]} />
-      < Piece2 number={numbers[2]} resource={resources[2]} color={colors[2]} />
+        < Piece0 number={numbers[0]} resource={resources[0]} color={colors[0]}/>
+        < Piece1 number={numbers[1]} resource={resources[1]} color={colors[1]} />
+        < Piece2 number={numbers[2]} resource={resources[2]} color={colors[2]} />
       </div>
-      <br></br>
       <div className="row">
-      < Piece3 number={numbers[3]} resource={resources[3]} color={colors[3]}/>
-      < Piece4 number={numbers[4]} resource={resources[4]} color={colors[4]}/>
-      < Piece5 number={numbers[5]} resource={resources[5]} color={colors[5]}/>
-      < Piece6 number={numbers[6]} resource={resources[6]} color={colors[6]}/>
+        < Piece3 number={numbers[3]} resource={resources[3]} color={colors[3]}/>
+        < Piece4 number={numbers[4]} resource={resources[4]} color={colors[4]}/>
+        < Piece5 number={numbers[5]} resource={resources[5]} color={colors[5]}/>
+        < Piece6 number={numbers[6]} resource={resources[6]} color={colors[6]}/>
       </div>
-      <br></br>
       <div className="row">
-      < Piece7 number={numbers[7]} resource={resources[7]} color={colors[7]}/>
-      < Piece8 number={numbers[8]} resource={resources[8]} color={colors[8]}/>
-      < Piece9 number={numbers[9]} resource={resources[9]} color={colors[9]}/>
-      < Piece10 number={numbers[10]} resource={resources[10]} color={colors[10]}/>
-      < Piece11 number={numbers[11]} resource={resources[11]} color={colors[11]}/>
+        < Piece7 number={numbers[7]} resource={resources[7]} color={colors[7]}/>
+        < Piece8 number={numbers[8]} resource={resources[8]} color={colors[8]}/>
+        < Piece9 number={numbers[9]} resource={resources[9]} color={colors[9]}/>
+        < Piece10 number={numbers[10]} resource={resources[10]} color={colors[10]}/>
+        < Piece11 number={numbers[11]} resource={resources[11]} color={colors[11]}/>
       </div>
-      <br></br>
       <div className="row">
-      < Piece12 number={numbers[12]} resource={resources[12]} color={colors[12]}/>
-      < Piece13 number={numbers[13]} resource={resources[13]} color={colors[13]}/>
-      < Piece14 number={numbers[14]} resource={resources[14]} color={colors[14]}/>
-      < Piece15 number={numbers[15]} resource={resources[15]} color={colors[15]}/>
+        < Piece12 number={numbers[12]} resource={resources[12]} color={colors[12]}/>
+        < Piece13 number={numbers[13]} resource={resources[13]} color={colors[13]}/>
+        < Piece14 number={numbers[14]} resource={resources[14]} color={colors[14]}/>
+        < Piece15 number={numbers[15]} resource={resources[15]} color={colors[15]}/>
       </div>
-      <br></br>
       <div className="row">
-      < Piece16 number={numbers[16]} resource={resources[16]} color={colors[16]}/>
-      < Piece17 number={numbers[17]} resource={resources[17]} color={colors[17]}/>
-      < Piece18 number={numbers[18]} resource={resources[18]} color={colors[18]}/>
+        < Piece16 number={numbers[16]} resource={resources[16]} color={colors[16]}/>
+        < Piece17 number={numbers[17]} resource={resources[17]} color={colors[17]}/>
+        < Piece18 number={numbers[18]} resource={resources[18]} color={colors[18]}/>
       </div>
     </div>
   );
